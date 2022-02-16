e14421f482f832875a9e5f791ff33f45b0c222f4
Rosen Georgiev <rosengeorgiew1@gmail.com>
2022-02-16 17:15:12 +0200

    Beginning the project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.scss'
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className='sidebar'>
+        <div className='main-content'>
+
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
