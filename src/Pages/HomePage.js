f9c10bac974bdef2ac57441008746cf83d779957
Rosen Georgiev <rosengeorgiew1@gmail.com>
2022-02-25 17:27:38 +0200

    homepage styles added

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
+import "../styles/typography.scss"
 
 function HomePage() {
     return (
-        <div>
-            <h1>HomePage</h1>
+        <div className='homepage'>
+            <header className='hero'>
+                <h1 className='hero-text'>
+                    Hi, I am
+                    <span> Rosen Georgiev</span>
+                </h1>
+                <p className='h-sub-text'>
+                    Lorem ipsum dolor sit amet.
+                </p>
+                <div className='icons'>
+                    <Link className='icon-holder' to="">
+                        <FontAwesomeIcon icon={faFacebook} className="icon" />
+                    </Link>
+                    <Link className='icon-holder' to="">
+                        <FontAwesomeIcon icon={faGithub} className="icon" />
+                    </Link>
+                    <Link className='icon-holder' to="">
+                        <FontAwesomeIcon icon={faLinkedin} className="icon" />
+                    </Link>
+                </div>
+            </header>
         </div>
     )
 }
