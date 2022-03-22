1399c8a5ed8c47b05225859f9018db5a1a0e9052
Rosen Georgiev <rosengeorgiew1@gmail.com>
2022-03-22 10:00:03 +0200

    ContactsPage build started

--- src/Pages/Contacts.js
+++ src/Pages/Contacts.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import ContactItem from '../Components/ContactItem';
+import phone from '../img/phone.svg';
+import email from '../img/emailme.svg';
+import location from '../img/location.svg';
 
 function Contacts() {
     return (
-        <div>
-            <h1>Contacts Page</h1>
+        <div className='contact-page'>
+            <div className='map-sect'></div>
+            <div className='contact-sect'>
+                <ContactItem icon={phone} text1={'+359 888031448'} title={'Phone'} />
+                <ContactItem icon={email} text1={'rosengeorgiew1@gmail.com'} title={'Email'} />
+                <ContactItem icon={location} text1={'Sofia, Bulgaria'} title={'Location'} />
+            </div>
         </div>
     )
 }
