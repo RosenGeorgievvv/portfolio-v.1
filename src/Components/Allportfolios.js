166a55923459a82994c0322d15c66d661f8b0f0f
Rosen Georgiev <rosengeorgiew1@gmail.com>
2022-03-22 12:15:13 +0200

    Portfolio page started

---
+++ src/Components/Allportfolios.js
@@ -0,0 +1,36 @@
+import amazoneClone from '../img/amazoneClone';
+import weatherApp from '../img/weatherApp';
+import newYearEve from '../img/newYearEve';
+
+const portfolios = [
+    {
+        id: 1,
+        category: 'website',
+        link1: 'https://github.com/RosenGeorgievvv/amazone-clone',
+        icon1: 'G',
+        icon2: 'Y',
+        image: amazoneClone,
+        title: 'Amazone Clone'
+    },
+    {
+        id: 2,
+        category: 'weather',
+        link1: 'https://github.com/RosenGeorgievvv/weather-app',
+        icon1: 'G',
+        icon2: 'Y',
+        image: weatherApp,
+        title: 'Weather Application'
+    },
+    {
+        id: 3,
+        category: 'counter',
+        link1: 'www.youtube.com',
+        link2: 'www.github.com',
+        icon1: 'G',
+        icon2: 'Y',
+        image: newYearEve,
+        title: 'New Year Event Counter'
+    }
+]
+
+export default portfolios;
--- src/Pages/Portfolio.js
+++ src/Pages/Portfolio.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import Categories from '../Components/Categories'
+import MenuItems from '../Components/MenuItems'
+import Title from '../Components/Title'
+import portfolios from '../Components/Allportfolios';
+import { useState } from 'react';
+
 
 function Portfolio() {
+    const [categories, setCategories] = useState(null);
+    const [menuItems, setMenuItems] = useState(portfolios);
+
+
+
     return (
-        <div>
-            <h1>Portfolio Page</h1>
+        <div className='portfolio-page'>
+            <div className='title'>
+                <Title title={'Portfolios'} span={'portfolios'} />
+            </div>
+            <div className='portfolios'>
+                <Categories />
+                <MenuItems menuItem={menuItems} />
+            </div>
         </div>
     )
 }
